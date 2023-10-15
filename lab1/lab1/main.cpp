eea701794b50eae3b02784c595f3acf5c89f40aa
melisrzgr <melisruzgar2001@gmail.com>
2023-10-15 20:56:56 +0300

    Dizinin elemanlarının çarpımını hesaplayan fonksiyon oluşturuldu

--- lab1/lab1/main.cpp
+++ lab1/lab1/main.cpp
@@ -9,3 +9,11 @@ int calculateSum(const int* numbers, int size) {
     }
     return sum;
 }
+// Dizinin elemanlarýnýn çarpýmýný hesaplayan fonksiyon
+int calculateProduct(const int* numbers, int size) {
+    int product = 1;
+    for (int i = 0; i < size; ++i) {
+        product *= numbers[i];
+    }
+    return product;
+}
