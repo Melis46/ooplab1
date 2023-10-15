ac8f191f70c70e752cbe332d9f2fcb59c80f4db7
melisrzgr <melisruzgar2001@gmail.com>
2023-10-15 20:52:48 +0300

    Dizinin elemanlarının toplamını hesaplayan fonksiyon oluşturuldu.

---
+++ lab1/lab1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+// Dizinin elemanlarýnýn toplamýný hesaplayan fonksiyon
+int calculateSum(const int* numbers, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += numbers[i];
+    }
+    return sum;
+}
